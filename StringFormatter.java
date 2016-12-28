12b989c83ee982b80c99da587b57f5072ac5493c
think <microhuang@live.com>
2016-12-28 22:24:52 +0800

    Update StringFormatter.java

--- StringFormatter.java
+++ StringFormatter.java
@@ -100,6 +100,84 @@ public class StringFormatter
         return fields;
     }
     
+public class StringFormatter 
+{
+    private String template="";
+    
+    /**
+     * 前端可用标签
+     */
+    private final Map<String,Map<String,Object>> labels = new HashMap();
+    
+    /**
+     * 添加可用标签
+     * @param label，前端标签
+     * @param value，前端默认值
+     * @return 
+     */
+    public boolean add(final String label, final Object value)
+    {
+        if(label==null || "".equals(label.trim()))
+            return false;
+        
+        if(!labels.containsKey(label))
+            labels.put(label.trim(), new HashMap(){{
+                put("index", labels.size()+1);//格式化用：%index$
+                put("label", label.trim());//前端用：%(name)$
+                put("default", value);//前端用，示例值
+            }});
+        
+        return true;
+    }
+    
+    /**
+     * 给标签设值
+     * @param label
+     * @param value
+     * @return 
+     */
+    public boolean set(final String label, final Object value)
+    {
+        if(label==null || "".equals(label.trim()))
+            return false;
+        
+        if(labels.containsKey(label.trim()))
+            labels.get(label.trim()).put("value", value);
+        
+        return true;
+    }
+    
+    public boolean template(final String temp)
+    {
+        template = temp;
+        return true;
+    }
+    
+    /**
+     * 得到所有可用标签
+     * @return 
+     */
+    public Map<String,Map<String,Object>> getLabels()
+    {
+        return labels;
+    }
+    
+    /**
+     * 得到实际使用标签
+     * @return 
+     */
+    public List getFields()
+    {
+        List fields = new ArrayList();
+        Pattern p = Pattern.compile("%\\((.*?)\\)");
+        Matcher m = p.matcher(template);
+        while(m.find()){
+            if(labels.containsKey(m.group(1)))
+                fields.add(m.group(1));
+        }
+        return fields;
+    }
+    
     /**
      * 处理模版字符
      * @param temp
@@ -112,11 +190,18 @@ public class StringFormatter
         Object[] params = new Object[labels.size()];
 //        temp = temp.replace("{", "").replace("}", "");//todo:xxx
         for (Map.Entry<String, Map<String, Object>> entry : labels.entrySet()) {
-            temp = temp.replace("("+entry.getKey()+")", entry.getValue().get("index")+"$");
+            temp = temp.replace("%("+entry.getKey()+")", "%"+entry.getValue().get("index")+"$");
             params[Integer.parseInt(entry.getValue().get("index").toString())-1] = entry.getValue().get("value");
         }
         //直接使用String格式化
-        return String.format(temp, params);
+        try
+        {
+            return String.format(temp, params);
+        }
+        catch(Exception ex)
+        {
+            return temp;
+        }
     }
     
     public String format()
